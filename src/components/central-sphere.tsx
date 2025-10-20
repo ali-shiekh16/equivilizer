6dcaf4fc80306d6d705a947028508a42bb6a6485
Ali <ali.shiekh16@outlook.com>
2025-10-21 02:52:58 +0500

    Update color & add sphere

--- src/components/central-sphere.tsx
+++ src/components/central-sphere.tsx
@@ -1,10 +1,12 @@
-// import { useRef } from 'react';
-// import { useFrame } from '@react-three/fiber';
-// import * as THREE from 'three';
+import { type JSX } from 'react';
 
 import { useTexture } from '@react-three/drei';
 
-const CentralSphere = () => {
+const CentralSphere = (
+  props: JSX.IntrinsicElements['group'] & {
+    color?: string;
+  }
+) => {
   // const orbitRef1 = useRef<THREE.Mesh>(null);
   // const orbitRef2 = useRef<THREE.Mesh>(null);
   // const orbitRef3 = useRef<THREE.Mesh>(null);
@@ -19,39 +21,22 @@ const CentralSphere = () => {
   const [emissive] = useTexture(['/texture/sphere-emissive.jpeg']);
 
   return (
-    <group position={[0, 0.1, 0]}>
-      {/* Three orbital patches */}
-      {/* <mesh ref={orbitRef1} position={[0, 0.2, 0]} rotation={[0, 0, 0]}>
-        <torusGeometry args={[0.22, 0.005, 2, 21, Math.PI / 3]} />
-        <meshStandardMaterial color='#ff0' metalness={0.0} roughness={0.8} />
+    <group {...props}>
+      <mesh position={[0, 0.2, 0]} castShadow receiveShadow>
+        <sphereGeometry args={[0.2, 32, 32]} />
+        <meshBasicMaterial color={'#000'} />
       </mesh>
-      <mesh
-        ref={orbitRef2}
-        position={[0, 0.2, 0]}
-        rotation={[0, 0, (Math.PI * 2) / 3]}
-      >
-        <torusGeometry args={[0.22, 0.005, 2, 21, Math.PI / 3]} />
-        <meshStandardMaterial color='#ff0' metalness={0.0} roughness={0.8} />
-      </mesh>
-      <mesh
-        ref={orbitRef3}
-        position={[0, 0.2, 0]}
-        rotation={[0, 0, (Math.PI * 4) / 3]}
-      >
-        <torusGeometry args={[0.22, 0.005, 2, 21, Math.PI / 3]} />
-        <meshStandardMaterial color='#ff0' metalness={0.0} roughness={0.8} />
-      </mesh> */}
 
-      {/* Main sphere */}
-      {/* <pointLight color='#f00' intensity={10} position={[0, 0.2, 0]} /> */}
       <mesh position={[0, 0.2, 0]} castShadow receiveShadow>
         <sphereGeometry args={[0.2, 32, 32]} />
         <meshStandardMaterial
           emissiveMap={emissive}
+          emissive={props.color ? props.color : '#41e620'}
+          emissiveIntensity={10}
           // emissiveIntensity={20}
           alphaMap={emissive}
           transparent={true}
-          color='#f00'
+          color={props.color || '#41e620'}
           // metalness={0.5}
           // roughness={0.8}
         />
--- src/scene.tsx
+++ src/scene.tsx
@@ -1,11 +1,15 @@
-import { OrbitControls } from '@react-three/drei';
+import { Environment, OrbitControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
-import { Bloom, EffectComposer, ToneMapping } from '@react-three/postprocessing';
+import {
+  Bloom,
+  EffectComposer,
+  ToneMapping,
+} from '@react-three/postprocessing';
 import { useControls } from 'leva';
 import Track from './components/track';
+import CentralSphere from './components/central-sphere';
 
 const Test = () => {
-
   const bloomControls = useControls('Bloom', {
     intensity: { value: 0.9, min: 0, max: 2, step: 0.1 },
     // kernelSize: { value: 3, min: 1, max: 5, step: 1 },
@@ -13,18 +17,18 @@ const Test = () => {
     luminanceSmoothing: { value: 0.03, min: 0, max: 0.1, step: 0.001 },
   });
 
-
   return (
     <Canvas
-    // gl={{
-    //   antialias: true,
-    // }}
+      gl={{
+        antialias: true,
+      }}
     >
-      {/* <Environment preset='studio' /> */}
       <ambientLight intensity={0.5} />
       <OrbitControls />
+      {/* <Environment preset='studio' /> */}
+      <CentralSphere position={[0, 0.3, 1.7]} scale={1} />
       <group position={[0, 0, 2]}>
-              <Track url={'/song.mp3'} color={'#41e620'} />
+        <Track url={'/song.mp3'} color={'#41e620'} />
       </group>
 
       <ambientLight intensity={0.3} />
